b80dc16ec872545f65cc1ee764fd0317cdb48315
BrokeWebDeveloper <53519952+BrokeWebDeveloper@users.noreply.github.com>
2019-10-14 13:10:33 +0530

    Create Playingaround.java

---
+++ Playingaround.java
@@ -0,0 +1,27 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    // Declaring String (With Value OF COURSE!
+    String Hobby = "Coding OF COURSE! ";
+    String Reason = "Because I Like The Sense Of Achieving Something! ";
+    String Errormessage = "That's Official! The IDE That I Use To Make This Console App Is Broken Because Some String's Value Is Corrupted. Please Try Later At https://helloworldabout.tk";
+    String Succeed = "Yay! It Works! If You Want More About The Developer Of The Code See Here : https://github.com/BrokeWebDeveloper";
+    // Declaring Variables With Number Values
+    int a = 4;
+    String Exampolo = "Exampolosa";
+        System.out.println ("My Hobby Is " + Hobby + " And Why Do I Code? " +
+			  Reason + "");
+    // Making A Conditions
+    if (a == 4)
+      {
+	    System.out.println ("Error : " + Errormessage + " For More Help.");
+      }
+
+    else
+      {
+	    System.out.println ("Succeed!" + Succeed + "");
+      }
+
+  }
+}
